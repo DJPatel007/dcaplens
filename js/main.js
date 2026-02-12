556850fed75203f08d693c57177596c034c784bc
Dhruvin Patel <39472283+DJPatel007@users.noreply.github.com>
2026-02-12 13:24:35 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,2 +1,7 @@
 // Placeholder for future animations or features
 console.log("DCapLens Photography loaded");
+
+
+function toggleMenu() {
+  document.getElementById("mobileMenu").classList.toggle("show");
+}
